936e545cfa7d44d6be592f1021979fdbaaac12e5
Vishal Patil <vpatil3@cisco.com>
2018-09-25 22:19:33 -0400

    Compile and test files

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mvn package && java -jar target/vrp-0.0.1-SNAPSHOT.jar
---
+++ test.sh
@@ -0,0 +1,1 @@
+curl -H 'Content-Type: application/json' -H 'Accept: application/json'   -X PUT -d "[\"location1\", \"location2\"]" http://localhost:8080/v1/VRP/addresses
