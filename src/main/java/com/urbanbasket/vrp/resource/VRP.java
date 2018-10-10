539881b8dc4b177f841995b864ccd0a3a1310f8f
Vishal Patil <vpatil3@cisco.com>
2018-10-09 21:15:35 -0400

    Checkpoint

--- src/main/java/com/urbanbasket/vrp/resource/VRP.java
+++ src/main/java/com/urbanbasket/vrp/resource/VRP.java
@@ -5,33 +5,47 @@ import com.google.maps.GeocodingApi;
 import com.google.maps.model.GeocodingResult;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.optaplanner.examples.vehiclerouting.domain.location.DistanceType;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.optaplanner.examples.vehiclerouting.domain.VehicleRoutingSolution;
-import org.optaplanner.examples.vehiclerouting.domain.location.RoadLocation;
-import org.optaplanner.examples.vehiclerouting.domain.location.Location;
 import org.optaplanner.examples.vehiclerouting.domain.Customer;
 import org.optaplanner.examples.vehiclerouting.domain.Depot;
 import org.optaplanner.examples.vehiclerouting.domain.Vehicle;
+import org.optaplanner.examples.vehiclerouting.domain.VehicleRoutingSolution;
+import org.optaplanner.examples.vehiclerouting.domain.location.DistanceType;
+import org.optaplanner.examples.vehiclerouting.domain.location.Location;
+import org.optaplanner.examples.vehiclerouting.domain.location.RoadLocation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+class VRPSolution {
 
-@RestController
-@RequestMapping("v1/VRP")
-public class VRP {
+    private static final Logger logger = LogManager.getLogger(VRPSolution.class);
+    private static final Integer CUSTOMER_DEMAND = 1;
+    private static final Integer VEHICLE_CAPACITY = 1000;
+    private final VrpParameters parameters;
+    private final VehicleRoutingSolution vrpSolution;
+    private final List<Location> roadLocations = new ArrayList<>();
+    private final List<Customer> customers = new ArrayList<>();
+    private final List<Vehicle> vehicles = new ArrayList<>();
+    private final List<Depot> depots = new ArrayList<>();
+    private Long locationId = new Long(0);
+    private Long depotID = null;
+    private GeoApiContext context = new GeoApiContext.Builder()
+            .apiKey(System.getenv("GOOGLE_MAPS_API_KEY"))
+            .build();
 
-    private static final Logger logger = LogManager.getLogger(VRP.class);
 
-    @GetMapping("healthz")
-    public @ResponseBody
-    ResponseEntity<String> healthz() {
-        return new ResponseEntity<>("ok", HttpStatus.OK);
+    VRPSolution(VrpParameters parameters) {
+        this.parameters = parameters;
+        this.vrpSolution = new VehicleRoutingSolution();
     }
 
+    private Long getNextRoadID() {
+        locationId += 1;
+        return locationId;
+    }
 
     private double getDistance(double lat1, double lon1, double lat2, double lon2) {
 
@@ -50,20 +64,13 @@ public class VRP {
         return Math.sqrt(distance);
     }
 
-    private static ArrayList<Location> solve(ArrayList<Location> unsortedLocations) {
-        ArrayList<Location>sortedLocations = new ArrayList();
-        VehicleRoutingSolution vrpSolution = new VehicleRoutingSolution();
-
-        return sortedLocations;
-    }
-
     private void calculateDistanceMatrix(List<Location> roadLocations) {
 
         roadLocations.stream().forEach(roadLocationObj -> {
-            RoadLocation roadLocation = (RoadLocation)roadLocationObj;
+            RoadLocation roadLocation = (RoadLocation) roadLocationObj;
 
             roadLocations.stream().forEach(neighboringRoadLocationObj -> {
-                RoadLocation neighboringRoadLocation = (RoadLocation)neighboringRoadLocationObj;
+                RoadLocation neighboringRoadLocation = (RoadLocation) neighboringRoadLocationObj;
                 if (roadLocation.getId() != neighboringRoadLocation.getId()) {
                     double distance = this.getDistance(roadLocation.getLatitude(), roadLocation.getLongitude(),
                             neighboringRoadLocation.getLatitude(), neighboringRoadLocation.getLongitude());
@@ -73,18 +80,53 @@ public class VRP {
         });
     }
 
-    @PutMapping("addresses")
-    public @ResponseBody
-    ResponseEntity<String[]> sortAddresses(@RequestBody VrpParameters parameters) {
+    private void setupCustomers() {
+        this.roadLocations.stream().forEach(roadLocation -> {
+            if (roadLocation.getId() != this.depotID) {
+                Customer customer = new Customer();
+                customer.setId(roadLocation.getId());
+                customer.setDemand(CUSTOMER_DEMAND);
+                customer.setLocation(roadLocation);
+                customers.add(customer);
+            }
+        });
 
-        VehicleRoutingSolution vrpSolution = new VehicleRoutingSolution();
-        List<Location> roadLocations = new ArrayList<>();
-        int id = 0;
+        this.vrpSolution.setCustomerList(this.customers);
+    }
 
+    private void setupVehicles() {
+        for (int i = 0; i < parameters.getCarCount(); i++) {
+            Vehicle vehicle = new Vehicle();
+            vehicle.setCapacity(VEHICLE_CAPACITY);
+            vehicles.add(vehicle);
+        }
+        this.vrpSolution.setVehicleList(vehicles);
+    }
+
+    private void setupDepots() {
+        try {
+            GeocodingResult[] results = GeocodingApi.geocode(context,
+                    parameters.getDepot()).await();
+
+            RoadLocation roadLocation = new RoadLocation();
+            roadLocation.setName(parameters.getDepot());
+            roadLocation.setLatitude(results[0].geometry.location.lat);
+            roadLocation.setLongitude(results[0].geometry.location.lng);
+            roadLocation.setId(this.getNextRoadID());
+            this.depotID = roadLocation.getId();
+
+            Depot depot = new Depot();
+            depot.setLocation(roadLocation);
+            depot.setId(this.depotID);
+            this.depots.add(depot);
+            vrpSolution.setDepotList(this.depots);
+        } catch (Exception exp) {
+            logger.error("Hit exception " + exp.toString());
+        }
+    }
+
+    private void setupRoadLocations() {
         for (String customerLocation : parameters.getCustomerLocations()) {
-            GeoApiContext context = new GeoApiContext.Builder()
-                    .apiKey(System.getenv("GOOGLE_MAPS_API_KEY"))
-                    .build();
             try {
                 GeocodingResult[] results = GeocodingApi.geocode(context,
                         customerLocation).await();
@@ -93,21 +135,49 @@ public class VRP {
                 roadLocation.setName(customerLocation);
                 roadLocation.setLatitude(results[0].geometry.location.lat);
                 roadLocation.setLongitude(results[0].geometry.location.lng);
-                roadLocation.setId(Long.valueOf(id));
-                roadLocations.add(roadLocation);
-
-                id += 1;
+                roadLocation.setId(this.getNextRoadID());
+                this.roadLocations.add(roadLocation);
             } catch (Exception exp) {
                 logger.error("Hit exception " + exp.toString());
             }
         }
 
-        this.calculateDistanceMatrix(roadLocations);
-
-        vrpSolution.setLocationList(roadLocations);
+        this.calculateDistanceMatrix(this.roadLocations);
+        vrpSolution.setLocationList(this.roadLocations);
         vrpSolution.setDistanceType(DistanceType.ROAD_DISTANCE);
+    }
 
-        return new ResponseEntity<>(roadLocations.toArray(new String[0]), HttpStatus.CREATED);
+    public void solve() {
+        this.setupRoadLocations();
+        this.setupDepots();
+        this.setupCustomers();
+        this.setupVehicles();
+    }
+}
+
+
+@RestController
+@RequestMapping("v1/VRP")
+public class VRP {
+
+    private static ArrayList<Location> solve(ArrayList<Location> unsortedLocations) {
+        ArrayList<Location> sortedLocations = new ArrayList();
+        VehicleRoutingSolution vrpSolution = new VehicleRoutingSolution();
+        return sortedLocations;
+    }
+
+    @GetMapping("healthz")
+    public @ResponseBody
+    ResponseEntity<String> healthz() {
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+    @PutMapping("addresses")
+    public @ResponseBody
+    ResponseEntity<String[]> sortAddresses(@RequestBody VrpParameters parameters) {
+        VRPSolution vrpSolution = new VRPSolution(parameters);
+        vrpSolution.solve();
+        return new ResponseEntity<>(parameters.getCustomerLocations(), HttpStatus.CREATED);
     }
 
 }
--- src/main/java/com/urbanbasket/vrp/resource/VrpParameters.java
+++ src/main/java/com/urbanbasket/vrp/resource/VrpParameters.java
@@ -1,16 +1,18 @@
 package com.urbanbasket.vrp.resource;
 
-public class VrpParameters {
+class VrpParameters {
     private String[] customerLocations;
     private String depot;
+    private Integer carCount;
 
     public VrpParameters() {
 
     }
 
-    public VrpParameters(String customerLocations[], String depot) {
+    public VrpParameters(String[] customerLocations, String depot, Integer carCount) {
         this.setCustomerLocations(customerLocations);
         this.setDepot(depot);
+        this.setCarCount(carCount);
     }
 
     public String[] getCustomerLocations() {
@@ -27,5 +29,13 @@ public class VrpParameters {
 
     public void setDepot(String depot) {
         this.depot = depot;
+    }
+
+    public Integer getCarCount() {
+        return carCount;
+    }
+
+    public void setCarCount(Integer carCount) {
+        this.carCount = carCount;
     }
 }
