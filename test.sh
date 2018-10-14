bad79c032f997517109fa560a31e7dc43f964eb8
Vishal Patil <vishpat@gmail.com>
2018-10-13 20:50:12 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
-locations="[\"300 constitution av apt 133, Bayonne, NJ 7002\",
-        \"17 new street, FL-1,    Bayonne,    NJ,    7002\",
-        \"3 Pamrapo CT Apt 3A,    Bayonne,    NJ,    7002\",
-        \"300 constitution ave apt 418,    Bayonne,    NJ,    7002\",
-        \"183 West 49th street Apt 3A,    Bayonne,    NJ,    7002\",
-         \"3 pamrapo ct Apt 3A,    Bayonne,    NJ,    7002\"]"
+locations="[\"Address 1\",
+        \"Address 2\",
+        \"Address 3\",
+        \"Address 4\",
+        \"Address 5\",
+         \"Address 6\"]"
 
-depot="\"180 Talmadge rd, Edison, Nj\""
+depot="\"UrbanBasket Address\""
 
 payload="{\"customerLocations\": $locations, \"depot\":$depot}"
 echo $payload | python -m json.tool
